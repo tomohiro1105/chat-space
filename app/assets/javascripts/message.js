c27cd4e30a2fde64245de3d5ae8be11ea3f2255d
tomohiro1105 <paripari500@icloud.com>
2019-10-29 17:25:12 +0900

    非同期更新の実装

---
+++ app/assets/javascripts/message.js
@@ -0,0 +1,46 @@
+$(document).on('turbolinks:load',function(){
+// $(function(){
+  function buildMessage(message){
+    var html = `<div class="message">
+                  <p class="message__user-name">
+                    ${message.username}
+                  </p>
+                  <p class="message__date">
+                    ${message.date}
+                  </p>
+                </div>
+                  <p class="message-text">
+                    ${message.content}
+                  </p>
+                  <img src='${ message.image ? message.image : ""}' class= "lower-message__image">
+                `
+    return html;
+  }
+
+  $('#new_message').on('submit', function(e){
+    e.preventDefault();
+    var formData = new FormData(this);
+    var url = $(this).attr('action');
+    $.ajax({
+      url: url,
+      type: "POST",
+      data: formData,
+      dataType: 'json',
+      processData: false,
+      contentType: false
+    })
+    
+    .done(function(message){
+      var post = buildMessage(message)
+      $(".chat-message").append(post);
+      $("#message_content").val('');
+      $(".chat-message").animate({scrollTop: $(".chat-message")[0].scrollHeight}, 'fast')
+    })
+    
+    .fail(function(){
+      alert("メッセージを入力してください");
+    });
+    return false; 
+  });
+});
+
